a9581952e138ba2c2e6f58dc5a8b4ee31cd18251
Iamdachi04 <dachi4github@gmail.com>
2025-08-27 23:52:33 +0400

    created the imageurl and search url , if you can see it closely , the search url has search in the place of discover , and also it has the query for whenever we want to address the searchbar for when something is given as an input in the searchbar.

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const API_URL='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ed10cc0b0082ae1de993727d8619446d'
 
 
+const image_path = "https://image.tmdb.org/t/p/w1280";
+
+
+const search_url = 'https://api.themoviedb.org/3/search/movie?api_key=ed10cc0b0082ae1de993727d8619446d&query=="'
+
+
+
+
